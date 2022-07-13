ffc01b94bc943c7b9e0f32e03a7190c524a5e78c
EfriaSembiring <epriasembiring@gmail.com>
2022-07-13 10:44:56 +0700

    edit dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,47 +17,40 @@
           color="primary"
       />
       <x-card-sum 
-          text="Jumlah Gudang" 
-          value="{{ $jumlah_gudang }}" 
-          icon="th" 
+          text="Transaksi Barang Masuk" 
+          value="{{ $barang_masuks }}" 
+          icon="download" 
           color="success"
       />
+
       <x-card-sum 
-          text="Keluar Masuk Barang" 
-          value="{{ $in_out }}" 
-          icon="chart-line" 
+          text="Transaksi Barang Keluar" 
+          value="{{ $barang_keluars }}" 
+          icon="upload" 
           color="danger"
       />
+
   </section>
 
   <section class="row">
       {{-- log activity section --}}
-      <div class="col-md-6">
-          <x-card>
-              <x-slot name="title">
-                  Statistik Gudang
-              </x-slot>
-              
-              <div class="chart-pie pt-4">
-                  <canvas id="myBarChart"></canvas>
-                </div>
-          </x-card>
-      </div>
-
-      {{-- chart section --}}
-      <div class="col-md-6">
-          <!-- Area Charts -->
-            <div class="card mb-4">
-              <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Statistik Barang</h6>
-              </div>
-              <div class="card-body">
-                <div class="chart-pie">
-                  <canvas id="myPieChart"></canvas>
-                </div>
-              </div>
+      
+      <div class="col-md-12">
+        <x-card>
+          <div class="card-header">
+            <div class="card-title"><h6 class="m-0 font-weight-bold text-primary">Jumlah Barang Masuk & Barang Keluar</h6></div>
+          </div>
+          
+          <div class="card-body">
+            <div class="chart-container" style="width: 100%; height: 80vh">
+              <canvas id="barChart"></canvas>
             </div>
+          </div>
+          
+        </x-card>
       </div>
+      
+
   </section>
 
   <section class="row">
@@ -96,152 +89,64 @@
   </section>
 
   <x-slot name="script">
-      <script>
-          // Set new default font family and font color to mimic Bootstrap's default styling
-          Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-          Chart.defaults.global.defaultFontColor = '#858796';
-          
-          // Bar Chart Example
-          var ctx = document.getElementById("myBarChart");
-          var myBarChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-              labels: [
-                  @foreach ($gudangs as $gudang)
-                      '{{ $gudang->nama }}',
-                  @endforeach
-              ],
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+   
+    <script>
+      const ctx = document.getElementById('barChart');
+      const barChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September',
+            'Oktober', 'November', 'Desember'],
               datasets: [{
-                label: "Jumlah",
-                backgroundColor: "#4e73df",
-                hoverBackgroundColor: "#2e59d9",
-                borderColor: "#4e73df",
-                data: [
-                  @foreach ($gudangs as $gudang)
-                      '{{ $gudang->barangs->sum('jumlah') }}',
-                  @endforeach
-                ],
-              }],
-            },
-            options: {
-              maintainAspectRatio: false,
-              layout: {
-                padding: {
-                  left: 10,
-                  right: 25,
-                  top: 25,
-                  bottom: 0
-                }
+                  label: 'Barang Masuk',
+                  data: [{{$masuk_jan}}, {{$masuk_feb}},{{$masuk_mar}}, {{$masuk_apr}}, {{$masuk_mei}},{{$masuk_jun}}, 
+                  {{$masuk_jul}}, {{$masuk_agu}},{{$masuk_sep}}, {{$masuk_okt}}, {{$masuk_nov}}, {{$masuk_des}} ],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      
+
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                     
+                  ],
+                  borderWidth: 1
               },
+              {
+                  label: 'Barang Keluar',
+                  data: [{{$keluar_jan}}, {{$keluar_feb}}, {{$keluar_mar}}, {{$keluar_apr}}, {{$keluar_mei}}, {{$keluar_jun}},
+                  {{$keluar_jul}}, {{$keluar_agu}}, {{$keluar_sep}}, {{$keluar_okt}}, {{$keluar_nov}}, {{$keluar_des}} ],
+                  backgroundColor: [
+                      'rgba(54, 162, 235, 0.2)'
+
+                  ],
+                  borderColor: [
+                      'rgba(54, 162, 235, 1)'
+  
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
               scales: {
-                xAxes: [{
-                  time: {
-                    unit: 'month'
-                  },
-                  gridLines: {
-                    display: false,
-                    drawBorder: false
-                  },
-                  ticks: {
-                    maxTicksLimit: 6
-                  },
-                  maxBarThickness: 25,
-                }],
-                yAxes: [{
-                  ticks: {
-                    min: 0,
-                    max: {{ $jumlah_barang }},
-                    maxTicksLimit: 5,
-                    padding: 10,
-                    // Include a dollar sign in the ticks
-                    callback: function(value, index, values) {
-                      return value;
-                    }
-                  },
-                  gridLines: {
-                    color: "rgb(234, 236, 244)",
-                    zeroLineColor: "rgb(234, 236, 244)",
-                    drawBorder: false,
-                    borderDash: [2],
-                    zeroLineBorderDash: [2]
+                  y: {
+                    
+                      beginAtZero: true
                   }
-                }],
-              },
-              legend: {
-                display: false
-              },
-              tooltips: {
-                titleMarginBottom: 10,
-                titleFontColor: '#6e707e',
-                titleFontSize: 14,
-                backgroundColor: "rgb(255,255,255)",
-                bodyFontColor: "#858796",
-                borderColor: '#dddfeb',
-                borderWidth: 1,
-                xPadding: 15,
-                yPadding: 15,
-                displayColors: false,
-                caretPadding: 10,
-                callbacks: {
-                  label: function(tooltipItem, chart) {
-                    var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                    return datasetLabel + ': ' + tooltipItem.yLabel;
-                  }
-                }
-              },
-            }
-          });
+              }
+          }
+      });
 
-          // Set new default font family and font color to mimic Bootstrap's default styling
-          Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-          Chart.defaults.global.defaultFontColor = '#858796';
+      // line chart
+     
+      
 
-          // Pie Chart Example
-          var ctx = document.getElementById("myPieChart");
-          var myPieChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-              labels: [
-                  @foreach ($barangs as $barang)
-                      '{{ $barang->nama }}',
-                  @endforeach
-              ],
-              datasets: [{
-                data: [
-                  @foreach ($barangs as $barang)
-                      {{ $barang->jumlah }},
-                  @endforeach
-                ],
-                backgroundColor: [
-                  @foreach ($barangs as $barang)
-                      @php
-                          $color = dechex(rand(0x000000, 0xFFFFFF));
-                      @endphp
-                      '{{ '#'.$color }}',
-                  @endforeach
-                ],
-                hoverBorderColor: "rgba(234, 236, 244, 1)",
-              }],
-            },
-            options: {
-              maintainAspectRatio: false,
-              tooltips: {
-                backgroundColor: "rgb(255,255,255)",
-                bodyFontColor: "#858796",
-                borderColor: '#dddfeb',
-                borderWidth: 1,
-                xPadding: 15,
-                yPadding: 15,
-                displayColors: false,
-                caretPadding: 10,
-              },
-              legend: {
-                display: false
-              },
-              cutoutPercentage: 80,
-            },
-          });
 
+
+
+  
       </script>
+      
   </x-slot>
 </x-app-layout>
